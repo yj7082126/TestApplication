6bbf151d8715a93b7ba88439510a11fd8d154e45
Patrick Kwon <yk7da@virginia.edu>
2017-01-28 22:20:48 -0500

    01-28-17 (2)

--- app/src/main/java/yongjaekwon/testapplication2/BucketItem.java
+++ app/src/main/java/yongjaekwon/testapplication2/BucketItem.java
@@ -1,14 +1,15 @@
 package yongjaekwon.testapplication2;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
 /**
  * Created by Kwon on 2017-01-27.
  */
-public class BucketItem {
+public class BucketItem implements Comparable<BucketItem>{
     private String mName;
     private Date mDate;
     private boolean mOnline;
@@ -33,12 +34,27 @@ public class BucketItem {
 
     public static ArrayList<BucketItem> createContactsList(int numContacts) throws ParseException {
         ArrayList<BucketItem> bucketItems = new ArrayList<BucketItem>();
-        Date date = new Date();
-        String dateFormat = "MM/dd/yyyy";
-        date = new SimpleDateFormat(dateFormat).parse("01/28/2017");
         for (int i = 0; i< numContacts; i++) {
+            long beginTime = Timestamp.valueOf("2013-01-01 00:00:00").getTime();
+            long endTime = Timestamp.valueOf("2013-12-31 00:58:00").getTime();
+            long diff = endTime - beginTime + 1;
+            Date date = new Date(beginTime + (long)(Math.random() * diff));
             bucketItems.add(new BucketItem("Person " + (++lastContactId), date, i <= numContacts/2));
         }
+        Collections.sort(bucketItems);
         return bucketItems;
+    }
+
+    @Override
+    public int compareTo(BucketItem o) {
+        if (mDate.getTime() > o.getDate().getTime()) {
+            return 1;
+        }
+        else if (mDate.getTime() < o.getDate().getTime()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
     }
 }
